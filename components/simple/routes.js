e125024bb2442b4820cc9e0b1b10448860fcda98
Christopher Florentino <C_Florentino2@yahoo.com>
2016-09-17 12:20:30 -0400

    Try again

--- components/simple/routes.js
+++ components/simple/routes.js
@@ -1,7 +1,7 @@
 (function(){
 	module.exports = {
 		helloWorld: function(req, res, next){
-			var body = "Hello World. I'm Chris";
+			var body = "Hello World! I'm Chris";
 			res.setHeader("Content-Type", "text/plain");
 			res.setHeader("Content-Length", body.length);
 			res.end(body);
